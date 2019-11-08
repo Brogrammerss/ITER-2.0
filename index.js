61676540a64fab9a6cac3e4571adcebc1d94ecb9
Anish Yadav <hney1000.ay1@gmail.com>
2019-11-08 19:34:24 +0530

    Added the routing

---
+++ index.js
@@ -0,0 +1,32 @@
+require('./config/config')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI ,{useNewUrlParser:true,useUnifiedTopology:true});
+var root = require('./routes/index');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+
+app.set('views',__dirname + '/public');
+app.engine('html',require('ejs').renderFile)
+app.set('view engine','ejs');
+
+
+
+app.use('/',root);
+
+app.use(express.static(__dirname+'/public'));
+
+
+var PORT = process.env.PORT 
+app.listen(PORT, () => {
+    console.log(`App started on port ${PORT}`)
+})
